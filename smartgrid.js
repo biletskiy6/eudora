9fd97c54dae38ef9ea55c3d0acda91a8bf949c48
biletskiy6 <victorbiletskiy82@gmail.com>
2019-02-11 14:46:50 +0200

    commit

--- smartgrid.js
+++ smartgrid.js
@@ -3,50 +3,53 @@ var smartgrid = require('smart-grid');
 var settings = {
     outputStyle: 'sass',
     columns: 12, 
-    offset: '30px', 
+    offset: '60px', 
     mobileFirst: false, 
     container: {
         maxWidth: '1520px', 
-        fields: '20px' 
+        fields: '30px' 
     },
     breakPoints: {
         lg: {
-            width: '1920px', 
-            fields: '20px'
+            width: '1680px', 
+            fields: '30px'
+        },
+        lgx: {
+            width: '1560px', 
+            fields: '30px'
         },
         desktop: {
             width: '1440px',
-            fields: '15px'
+            fields: '30px'
         },
         md: {
             width: '1366px',
-            fields: '15px'
+            fields: '30px'
 
         },
         mdx: {
             width: '1280px',
-            fields: '15px'
+            fields: '30px'
 
         },
         tablet: {
-            width: '1024px',
-            fields: '15px'
+            width: '992px',
+            fields: '30px'
 
         },
         tabletx: {
             width: '768px',
-            fields: '15px'
+            fields: '30px'
 
+        },
+        phoneh: {
+            width: '576px',
+            fields: '30px'
         },
         phone: {
             width: '480px',
-            fields: '10px'
+            fields: '30px'
         },
-        iphone: {
-            width: '320px',
-            fields: '10px'
-        }
-
     }
 };
 
--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -1,11 +1,21 @@
 $(function(){
 	svg4everybody();
-		$(".food-content").hide();
+/*		$(".food-content").hide();
 	$(".food-item").hover(function() {
 		$(this).find($(".food-content")).show();
 	}, function() {
 		$(this).find($(".food-content")).hide();
-	});
+	});*/
+
+	  $(".toggle-menu").on("click", function() {
+    $(this).toggleClass("toggle-menu--active");
+    $(".main-navigation").toggleClass("active");
+    $(".intro").toggleClass("layer-change");
+
+
+
+
+  });
 
   $('ul.foods-tabs').on('click', 'li:not(.food-active)', function(e) {
     e.preventDefault();
@@ -13,4 +23,49 @@ $(function(){
       .addClass('food-active').siblings().removeClass('food-active')
       .closest('div.foods').find('div.foods__content').removeClass('active').eq($(this).index()).addClass('active');
   });
+
+     
+  	$(".main-menu li a").on("click", function() {
+  		 if($(".main-navigation").hasClass("active")) { 
+  		 	$(".main-navigation").removeClass("active");
+  		 	$(".toggle-menu").removeClass("toggle-menu--active");
+  		 	if ($('.intro').hasClass('layer-change')) {
+            $('.intro').removeClass('layer-change');
+        }
+  		 }
+  		
+  });
+
+  	$(".food-item .secondary-button").on("click", function(e) {
+  		e.preventDefault();
+  		$(this).parent().find(".food-content").toggleClass("active");
+  		$(this).parent().find(".food-img").toggleClass("active");
+  	});
+
+
+});
+
+$(window).on('resize', function() {
+    var win = $(this);
+    if (win.width() >= 1280) {
+        $('.main-menu ul').removeAttr('style');
+        if ($('.toggle-menu').hasClass('toggle-menu--active')) {
+            $('.toggle-menu').removeClass('toggle-menu--active');
+        }
+        if ($('.intro').hasClass('layer-change')) {
+            $('.intro').removeClass('layer-change');
+        }
+
+        if ($('.main-navigation').hasClass('active')) {
+            $('.main-navigation').removeClass('active');
+        }
+    }
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1) {
+    $('.top-line').addClass('top-line--fixed');
+  } else {
+    $('.top-line').removeClass('top-line--fixed');
+  }
 });
